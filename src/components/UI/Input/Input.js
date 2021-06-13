0b68f899bd27c10f7e12909f274649d1b8ea960b
Anirup Patnaik <anirup049@gmail.com>
2021-06-13 17:28:38 +0530

    Calculator Input UI Design

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,11 @@
+import classes from './Input.module.css';
+
+const Input = () => {
+    return (
+        
+        <input className={classes.input} />
+        
+    );
+}
+
+export default Input;
