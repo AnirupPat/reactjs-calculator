e15c4ccef9e0c8907f78a812859f189b55e0cc47
Anirup Patnaik <anirup049@gmail.com>
2021-06-13 18:08:23 +0530

    Base Component for VirtualKeyPad

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,15 @@
-import classes from './Calculator.module.css';
-import Card from './UI/Card/Card';
-import Input from './UI/Input/Input';
+import classes from "./Calculator.module.css";
+import Card from "./UI/Card/Card";
+import Input from "./UI/Input/Input";
+import VirtualKeyPad from "./VirtualKeyPad/VirtualKeyPad";
 
 const Calculator = () => {
-    return (
-        <Card className={classes.calculator}>
-            <Input />
-        </Card>
-    );
-}
+  return (
+    <Card className={classes.calculator}>
+      <Input />
+      <VirtualKeyPad />
+    </Card>
+  );
+};
 
 export default Calculator;
---
+++ src/components/VirtualKeyPad/VirtualKeyPad.js
@@ -0,0 +1,12 @@
+import classes from './VirtualKeyPad.module.css';
+import Card from '../UI/Card/Card';
+
+const VirtualKeyPad = () => {
+    return (
+        <Card className={classes.virtualKeyPad}>
+
+        </Card>
+    );
+}
+
+export default VirtualKeyPad;
