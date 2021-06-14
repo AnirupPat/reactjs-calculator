38bfb8da335c34ac301046288842af5546dfe8f1
Anirup Patnaik <anirup049@gmail.com>
2021-06-14 12:48:19 +0530

    Adding Bump on click of the virtual keypad

--- src/components/UI/NumberButton/NumberButton.js
+++ src/components/UI/NumberButton/NumberButton.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import classes from "./NumberButton.module.css";
 
 const NumberButton = (props) => {
-  const numberClass = `${classes.button} ${props.className}`;
-  return <button className={numberClass}>{props.number}</button>;
+  const [buttonHighlighted, setButtonHighlighted] = useState(false);
+  const buttonClickHandler = (event) => {
+    event.preventDefault();
+    console.log(event.target.innerHTML);
+    setButtonHighlighted(true);
+    setTimeout(() => {
+      setButtonHighlighted(false);
+    }, 300);
+  };
+
+  const numberClass = `${classes.button} ${props.className} ${
+    buttonHighlighted ? classes.bump : ""
+  }`;
+  return (
+    <button onClick={buttonClickHandler} className={numberClass}>
+      {props.number}
+    </button>
+  );
 };
 
 export default NumberButton;
